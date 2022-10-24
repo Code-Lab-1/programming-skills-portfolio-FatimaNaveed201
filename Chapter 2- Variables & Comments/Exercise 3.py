7795a425ab5aa6a781fe8f701441b18f4cfbb226
Fatima Naveed <fatimanaveed201@gmail.com>
2022-10-24 10:59:40 +0400

    Create Exercise 3.py

---
+++ Chapter 2- Variables & Comments/Exercise 3.py
@@ -0,0 +1,15 @@
+# Exercise 3: Stripping Names 
+
+name = "\n\tFatima Naveed\n"
+
+print ("Name: ")
+print(name)
+
+print ("\nby using lstrip():")
+print (name.lstrip())
+
+print ("\nby using rstrip():")
+print (name.rstrip())
+
+print ("\nby using strip():")
+print (name.strip())
