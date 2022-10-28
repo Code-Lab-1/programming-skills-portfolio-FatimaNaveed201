d230eb4be12dd7c5a0e16353e170be05b0fa797f
Fatima Naveed <fatimanaveed201@gmail.com>
2022-10-28 20:50:05 +0400

    Create Exercise 5.py

---
+++ Chapter 4- Control Flow/Exercise 5.py
@@ -0,0 +1,14 @@
+# Exercise 5: Favorite Fruit 
+
+favorite_fruits = ["Mangos", "Lychees", "Strawberries"]
+
+if "Watermelons" in favorite_fruits:
+    print ("I love watermelons!!")
+if "Lychees" in favorite_fruits:
+    print ("I love Lychees!!")
+if "Cherries" in favorite_fruits:
+    print ("I love Cherries!!")
+if "Mangos" in favorite_fruits:
+    print ("I love Mangos!!")
+if "Strawberries" in favorite_fruits:
+    print ("I love Strawberries!!")
