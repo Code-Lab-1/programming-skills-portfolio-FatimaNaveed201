81701d9b7e0400cea51b47e05a4128878ee71366
Fatima Naveed <fatimanaveed201@gmail.com>
2022-11-20 18:46:54 +0400

    Create Exercise 1.py

---
+++ Chapter 5- Dictionaries/Exercise 1.py
@@ -0,0 +1,8 @@
+# Exercise 1: Person 
+
+information = {"first_name": "Farah", "last_name": "Naveed","age": "44","city": "Sharjah",}
+
+print (information["first_name"])
+print (information["last_name"])
+print (information["age"])
+print (information["city"])
