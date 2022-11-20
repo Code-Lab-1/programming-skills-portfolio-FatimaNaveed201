22d7d7db06c27e32db47b3004079968210a96f4c
Fatima Naveed <fatimanaveed201@gmail.com>
2022-11-20 18:47:17 +0400

    Create Exercise 2.py

---
+++ Chapter 6- Loops/Exercise 2.py
@@ -0,0 +1,18 @@
+# Exercise 2: Movie Ticket
+
+task = "How old are you?"
+task += "\nEnter 'quit' when you are done. "
+
+while True:
+    age = input(task)
+    if age == 'quit':
+        break
+    
+    age = int(age)
+
+    if age < 3:
+        print ("Your ticket is free!!")
+    elif age < 13:
+        print("Your ticket costs $10. ")
+    else: 
+        print("Your ticket costs $15. ")
