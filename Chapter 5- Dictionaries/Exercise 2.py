3320df0921dad25c8297bf379503075604c251a0
Fatima Naveed <fatimanaveed201@gmail.com>
2022-11-20 18:46:59 +0400

    Create Exercise 2.py

---
+++ Chapter 5- Dictionaries/Exercise 2.py
@@ -0,0 +1,22 @@
+# Exercise 2: Glossary 
+
+Glossary = {"Comment":"A python comment is a line of text that appears in a program but is not executed. Comments are used to explain the code.",
+"Escape character":"It is used to insert the special characters in a string. It is a backslash \ followed by the special character", 
+"print()":"The print function is used to print the string or value",
+"Boolean":"Booleans represnt one of the two values i.e., True and False",
+"Lists":"lists in python are used to store multiple values and items in a single variable. A list is an ordered and changeable collection of data."}
+
+word = "Comment"
+print ("\n" + word.title() + ": " + Glossary[word])
+
+word = "Escape character"
+print ("\n" + word.title() + ": " + Glossary[word])
+
+word = "print()"
+print ("\n" + word.title() + ": " + Glossary[word])
+
+word = "Boolean"
+print ("\n" + word.title() + ": " + Glossary[word])
+
+word = "Lists"
+print ("\n" + word.title() + ": " + Glossary[word])
