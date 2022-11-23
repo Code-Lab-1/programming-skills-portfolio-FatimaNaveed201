47bc9c2aa2a123a96dd10342283e6c2e15ad0c03
Fatima Naveed <fatimanaveed201@gmail.com>
2022-11-24 00:06:50 +0400

    Create Exercise 7.py

---
+++ Chapter 4- Control Flow/Exercise 7.py
@@ -0,0 +1,9 @@
+#Sales Record 
+
+sales = int(input())
+if sales >= 50000:
+    print("You get a bonus of $500.")
+else:
+    print("Sorry you are not eligible for a bonus due to less sales.")
+
+print()
