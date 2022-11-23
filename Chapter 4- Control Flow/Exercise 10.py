02c249ef6ca674769673043d4280b9e242c630ac
Fatima Naveed <fatimanaveed201@gmail.com>
2022-11-24 00:06:40 +0400

    Create Exercise 10.py

---
+++ Chapter 4- Control Flow/Exercise 10.py
@@ -0,0 +1,9 @@
+#Check wether the given alphabet is a vowel or not
+
+letter = input("Enter an alphabet : ")
+if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u":
+    print("The given alphabet is a vowel.")
+elif letter == "A" or letter == "E" or letter == "I" or letter == "O" or letter == "U":
+    print("The given alphabet is a vowel.")
+else: 
+    print("The given alphabet is not a vowel.")
