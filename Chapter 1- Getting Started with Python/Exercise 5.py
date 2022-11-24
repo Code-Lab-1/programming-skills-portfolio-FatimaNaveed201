757fb8a3a3512041a29c3d75b5e44896eeceedcb
Fatima Naveed <fatimanaveed201@gmail.com>
2022-11-24 10:58:41 +0400

    Update Exercise 5.py

--- Chapter 1- Getting Started with Python/Exercise 5.py
+++ Chapter 1- Getting Started with Python/Exercise 5.py
@@ -2,4 +2,4 @@
 
 from math import pi
 r = float(input("Input the radius of the circle: "))
-print ("The are of the circle with the radius" + str(r) + "is" + str(pi*r**2))
+print ("The are of the circle with the radius " + str(r) + "is " + str(pi*r**2))
